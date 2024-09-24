f42a385ae51c34e9cc2d8ddefc3dfb79b57215e7
Wasif-khan19 <wk5599426@gmail.com>
2024-09-24 06:17:39 +0500

    edited

--- models/ecommerce/product.models.js
+++ models/ecommerce/product.models.js
@@ -8,6 +8,27 @@ const productSchema = new mongoose.Schema({
     description: {
         type: String,
         required: true
+    },
+    productImage: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    stock: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true,
+    },
+    brand: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
     }
 }, { timestamps: true })
 
