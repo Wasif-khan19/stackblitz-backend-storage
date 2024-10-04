55eec00fff63247ef9034faa7fba635dee89bd2b
Wasif-khan19 <wk5599426@gmail.com>
2024-10-04 11:15:55 +0500

    edited

--- models/hospitalManagement/patient.model.js
+++ models/hospitalManagement/patient.model.js
@@ -1,7 +1,22 @@
 import mongoose, { Mongoose } from 'mongoose'
 
 const patientSchema = new Mongoose.Schema({
-
+    name:{
+        type:String,
+        required:true
+    },
+    bloodGroup:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    problem:{
+        type:String,
+        required:true
+    }
 },{timestamps: true})
 
 export const patientModel = mongoose.Model("patient", patientSchema)
