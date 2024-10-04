e0e6231a2d27a36150e169584deed423f7d2384c
Wasif-khan19 <wk5599426@gmail.com>
2024-10-04 11:11:18 +0500

    edited

--- models/hospitalManagement/doctor.models.js
+++ models/hospitalManagement/doctor.models.js
@@ -1,7 +1,29 @@
 import mongoose from 'mongoose'
 
 const doctorSchema = new mongoose.Schema({
-    
+    name:{
+        type:String,
+        required: true
+    },
+    salary:{
+        type:String,
+        required:true
+    },
+    workHospitalName:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"hospitalschema"
+        }
+    ],
+    experience:{
+        type:Number,
+        required:true
+    },
+    qualification:{
+        type:String,
+        required:true
+    },
+
 },{timestamps: true})
 
 export const doctorModel = mongoose.Model("doctormodel", doctorSchema)
--- models/hospitalManagement/hospital.models.js
+++ models/hospitalManagement/hospital.models.js
@@ -1,5 +1,18 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const hospitalSchema = new Mongoose.Schema({},{timestamps: true})
+const hospitalSchema = new Mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    specialty:{
+        type:String
+    },
+    
+},{timestamps: true})
 
 export const hospitalModel = Mongoose.Model("hospitalschema", hospitalSchema)
